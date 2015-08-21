403db9ec4b7809458e0dfed3f1d951548a4a56a8
Konstantin <extertioner@yandex.ru>
2015-08-21 10:24:50 +0300

    fix simplejson import in treeadmin/admin.py

--- treeadmin/admin.py
+++ treeadmin/admin.py
@@ -3,13 +3,17 @@ from django.contrib import admin
 from django.contrib.admin.views import main
 from django.db.models import Q
 from django.http import HttpResponse, HttpResponseBadRequest, HttpResponseForbidden, HttpResponseNotFound, HttpResponseServerError
-from django.utils import simplejson
 from django.utils.safestring import mark_safe
 from django.utils.translation import ugettext_lazy as _, ugettext
 
 from mptt.exceptions import InvalidMove
 
 import logging
+
+try:
+    from django.utils import simplejson
+except ImportError:
+    import json as simplejson
 
 
 # ------------------------------------------------------------------------
